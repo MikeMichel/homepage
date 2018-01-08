9b2a9b37f1c16f06bc9ec497110df953098cc344
Mike Michel <mike.michel@int.sevenval.com>
2018-01-08 15:09:02 +0100

    cli

---
+++ gatsby-config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  siteMetadata: {
+    title: `Gatsby with Contentful`,
+  },
+  plugins: [
+    {
+      resolve: `gatsby-source-contentful`,
+      options: {
+        spaceId: process.env.CONTENTFUL_SPACE_ID || '',
+        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN || '',
+      },
+    },
+    `gatsby-transformer-remark`,
+  ],
+}
